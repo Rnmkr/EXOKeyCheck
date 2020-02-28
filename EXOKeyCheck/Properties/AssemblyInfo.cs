1ee0e68aea1aba17391e22df9170134603203176
Jose Pompilio <milveintiuno@gmail.com>
2020-02-28 11:56:51 -0300

    beta 1

--- EXOKeyCheck/Properties/AssemblyInfo.cs
+++ EXOKeyCheck/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EXOKeyCheck")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Check de Licencias Consumidas")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("EXO")]
 [assembly: AssemblyProduct("EXOKeyCheck")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright ©  2020 by Rnmkr")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
