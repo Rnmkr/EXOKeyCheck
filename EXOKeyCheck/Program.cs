433c2ff5c43f9bcaabdd555de6e0e10f489f7ba3
Jose Pompilio <milveintiuno@gmail.com>
2020-10-15 12:58:58 -0300

    todo nuevo...

--- EXOKeyCheck/Program.cs
+++ EXOKeyCheck/Program.cs
@@ -4,49 +4,47 @@ using System.Data.SqlClient;
 using System.IO;
 using System.Text.RegularExpressions;
 
+//connectionString = @"data source=VM-FORREST;initial catalog=EXOOAKeys2020;persist security info=True;user id=BUBBASQL;password=12345678;MultipleActiveResultSets=True";
+//connectionString = @"data source=DESKTOP;initial catalog=EXOOAKeys2020; integrated security=True; MultipleActiveResultSets=True";
+
 namespace EXOKeyCheck
 {
-    /// <summary>
-    //<Key>
-    //  <ProductKey>NFX7C-HRFRM-J6VJK-FDHG3-VXMMP</ProductKey>
-    //  <ProductKeyID>3273088776052</ProductKeyID>
-    //  <ProductKeyState>2</ProductKeyState>
-    //  <ProductKeyPartNumber>KU9-00001</ProductKeyPartNumber>
-    //</Key>
-    //var productKey = args[1].ToUpper();
-    //var serialNumber = args[2].ToUpper();
-    //var productKeyID = args[3];
-    //var productKeyPartNumber = args[4].ToUpper();
-    /// </summary>
-
-    //TODO: purgar, pasar a ProductKey y usar el nuevo branch
     class Program
     {
         static void Main(string[] args)
         {
             try
             {
+                args[1] = args[1].ToUpper();
+                args[2] = args[2].ToUpper();
+                args[4] = args[5].ToUpper();
+
                 switch (args[0])
                 {
                     case "/?":
                         ShowHelp();
                         break;
                     case "/sk":
-                        if (ArgsValidation(args))
-                        {
-                            SaveKeyToDatabase(args[1], args[2], args[3], args[4]);
-                        }
+                        SaveKeyToDatabase(args);
                         break;
                     case "/hk":
-                        if (ArgsValidation(args))
-                        {
-                            SetBoundState(args[1], args[2]);
-                        }
+                        SetBoundState(args);
                         break;
                     default:
+                        Console.WriteLine("Error: Opción inválida." + Environment.NewLine + "Utilice '/?' para ver la ayuda.");
                         Environment.Exit(-1);
                         break;
                 }
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Error: No se permiten parámetros núlos o cadena de espacios vacías.");
+                Environment.Exit(-1);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Error: la cantidad de parámetros es inválida." + Environment.NewLine + "Utilice '/?' para ver la ayuda.");
+                Environment.Exit(-1);
             }
             catch (Exception e)
             {
@@ -55,88 +53,142 @@ namespace EXOKeyCheck
             }
         }
 
-        private static bool ArgsValidation(string[] args)
+        private static void SaveKeyToDatabase(string[] args)
         {
-            if (args.Length < 4) return false;
-
-            foreach (var arg in args)
+            if (IsArgsValid(args))
             {
-                if (string.IsNullOrWhiteSpace(arg)) return false;
+                ExecuteSaveQuery(args);
             }
+        }
 
-            var productKey = args[1].ToUpper();
-            var serialNumber = args[2].ToUpper();
+        private static void SetBoundState(string[] args)
+        {
+            if (IsArgsValid(args))
+                throw new NotImplementedException();
+        }
+
+        private static void ExecuteSaveQuery(string[] args)
+        {
+            var serialNumber = args[1];
+            var productKey = args[2];
             var productKeyID = args[3];
-            var productKeyPartNumber = args[4].ToUpper();
+            var productKeyState = args[4];
+            var productKeyPartNumber = args[5];
+
+            var connectionString = @"data source=BUBBA;initial catalog=EXOOAKeys2020;persist security info=True;user id=BUBBASQL;password=12345678;MultipleActiveResultSets=True;";
+            var sqlConnection = new SqlConnection(connectionString);
+
+            using (sqlConnection)
+            {
+                try
+                {
+                    sqlConnection.Open();
+                    var sqlCommand = new SqlCommand("SaveProductKey", sqlConnection)
+                    {
+                        CommandType = CommandType.StoredProcedure
+                    };
+
+                    sqlCommand.Parameters.AddWithValue("@SerialNumber", serialNumber);
+                    sqlCommand.Parameters.AddWithValue("@ProductKey", productKey);
+                    sqlCommand.Parameters.AddWithValue("@ProductKeyID", productKeyID);
+                    sqlCommand.Parameters.AddWithValue("@ProductKeyState", productKeyState);
+                    sqlCommand.Parameters.AddWithValue("@ProductKeyPartNumber", productKeyPartNumber);
+                    sqlCommand.Parameters.Add("@Output", SqlDbType.Char, 500);
+                    sqlCommand.Parameters["@Output"].Direction = ParameterDirection.Output;
+                    sqlCommand.ExecuteNonQuery();
+
+                    var sqloutput = Convert.ToInt32(sqlCommand.Parameters["@Output"].Value);
+                    //if (sqloutput != 0)
+                    //{
+                    //    var report = ExecuteSelectByProductKeyQuery(productKey, "SelectByProductKey");
+
+                    //    //para el test de eze
+                    //    if (report.State == "Consumed")
+                    //        File.Create(AppDomain.CurrentDomain.BaseDirectory + "Estado.Consumed");
+
+                    //    if (report.State == "Bound")
+                    //        File.Create(AppDomain.CurrentDomain.BaseDirectory + "Estado.Bound");
+                    //}
+                    Environment.Exit(sqloutput);
+                }
+                catch (Exception e)
+                {
+                    if (e.Message.Contains("network-related"))
+                    {
+                        //Console.WriteLine("Error en la red intentando guardar registro." + Environment.NewLine + e.Message);
+                        Environment.Exit(-8);
+                    }
+                    //Console.WriteLine("Error intentando guardar registro." + Environment.NewLine + e.Message);
+                }
+                finally
+                {
+                    sqlConnection.Close();
+                }
+            }
+        }
+
+        private static bool IsArgsValid(string[] args)
+        {
+            foreach (var arg in args)
+                if (string.IsNullOrWhiteSpace(arg))
+                    throw new ArgumentNullException();
 
             Regex regexForProductKey = new Regex(@"^([A-Za-z0-9]{5}-){4}[A-Za-z0-9]{5}$");
-            if (!regexForProductKey.IsMatch(productKey))
-            {
-                throw new ArgumentException("Error: ProductKey inválido [1]");
-            }
+            if (!regexForProductKey.IsMatch(args[1]))
+                throw new ArgumentException("Error: El parámetro (1) ProductKey es inválido.");
+
 
             Regex regexForSerialNumber = new Regex(@"^\d{7}[a-gA-G]\d{5}$");
-            if (!regexForSerialNumber.IsMatch(serialNumber))
-            {
-                throw new ArgumentException("Error: SerialNumber inválido [2]");
-            }
+            if (!regexForSerialNumber.IsMatch(args[2]))
+                throw new ArgumentException("Error: El parámetro (2) SerialNumber es inválido.");
 
-            Regex regexForProductKeyID = new Regex(@"^[0-9]+$"); ///
-            if (!regexForProductKeyID.IsMatch(productKeyID))
-            {
-                throw new ArgumentException("Error: ProductKeyID inválido [3]");
-            }
 
-            Regex regexForProductKeyPartNumber = new Regex(@"^([A-Za-z0-9]{3}-)[0-9]{5}$"); ///
-            if (!regexForProductKeyPartNumber.IsMatch(productKeyPartNumber))
-            {
-                throw new ArgumentException("Error: ProductKeyPartNumber inválido [4]");
-            }
+            Regex regexForProductKeyID = new Regex(@"^[0-9]+$");
+            if (!regexForProductKeyID.IsMatch(args[3]))
+                throw new ArgumentException("Error: El parámetro (3) ProductKeyID es inválido.");
+
+
+            Regex regexForProductKeyPartNumber = new Regex(@"^([A-Za-z0-9]{3}-)[0-9]{5}$");
+            if (!regexForProductKeyPartNumber.IsMatch(args[4]))
+                throw new ArgumentException("Error: El parámetro (4) ProductKeyPartNumber es inválido.");
 
             return true;
         }
 
-        private static void SetBoundState(string v1, string v2)
-        {
-            throw new NotImplementedException();
-        }
 
-        private static void SaveKeyToDatabase(string productKey, string serialNumber, string productKeyID, string productKeyPartNumber)
-        {
-            throw new NotImplementedException();
-        }
 
-        private static void SelectQuery(string option, string key, string serialNumber)
-        {
-            Regex regexKey = new Regex(@"^([A-Za-z0-9]{5}-){4}[A-Za-z0-9]{5}$");
-            if (!regexKey.IsMatch(key))
-            {
-                throw new ArgumentException("Formato inválido de OA Key");
-            }
 
-            Regex regexSerialNumber = new Regex(@"^\d{7}[a-gA-G]\d{5}$");
-            if (!regexSerialNumber.IsMatch(serialNumber))
-            {
-                throw new ArgumentException("Formato inválido de Serial Number");
-            }
 
-            switch (option)
-            {
-                case "/sk":
-                    TrySaveKey(key, serialNumber);
-                    break;
-                case "/hk":
-                    TryHashKey(key, serialNumber);
-                    break;
-                default:
-                    throw new ArgumentException("Opcion inválida");
-            }
-        }
 
-        private static void TrySaveKey(string key, string serialNumber)
-        {
-            ExecuteSaveQuery(key, serialNumber);
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         private static void TryHashKey(string key, string serialNumber)
         {
@@ -170,8 +222,6 @@ namespace EXOKeyCheck
         private static void ExecuteHashQuery(int reportID)
         {
             var connectionString = @"data source=BUBBA;initial catalog=EXOOAKeys2020;persist security info=True;user id=BUBBASQL;password=12345678;MultipleActiveResultSets=True;";
-            //var connectionString = @"data source=VM-FORREST;initial catalog=EXOOAKeys2020;persist security info=True;user id=BUBBASQL;password=12345678;MultipleActiveResultSets=True";
-            //var connectionString = @"data source=DESKTOP;initial catalog=EXOOAKeys2020; integrated security=True; MultipleActiveResultSets=True";
             var sqlConnection = new SqlConnection(connectionString);
 
             using (sqlConnection)
@@ -261,68 +311,34 @@ namespace EXOKeyCheck
 
         }
 
-        private static void ExecuteSaveQuery(string key, string serialNumber)
-        {
-            var connectionString = @"data source=BUBBA;initial catalog=EXOOAKeys2020;persist security info=True;user id=BUBBASQL;password=12345678;MultipleActiveResultSets=True;";
-            //var connectionString = @"data source=VM-FORREST;initial catalog=EXOOAKeys2020;persist security info=True;user id=BUBBASQL;password=12345678;MultipleActiveResultSets=True";
-            //var connectionString = @"data source=DESKTOP;initial catalog=EXOOAKeys2020; integrated security=True; MultipleActiveResultSets=True";
-            var sqlConnection = new SqlConnection(connectionString);
-            using (sqlConnection)
-            {
-                try
-                {
-                    sqlConnection.Open();
-                    var sqlCommand = new SqlCommand("SaveOAKey", sqlConnection)
-                    {
-                        CommandType = CommandType.StoredProcedure
-                    };
-                    //sqlCommand.Parameters.AddWithValue("@ProductKeyCode", productkeycode);
-                    sqlCommand.Parameters.AddWithValue("@OAKey", key);
-                    sqlCommand.Parameters.AddWithValue("@SerialNumber", serialNumber);
-                    sqlCommand.Parameters.Add("@Output", SqlDbType.Char, 500);
-                    sqlCommand.Parameters["@Output"].Direction = ParameterDirection.Output;
-                    sqlCommand.ExecuteNonQuery();
 
-                    var sqloutput = Convert.ToInt32(sqlCommand.Parameters["@Output"].Value);
-                    if (sqloutput != 0)
-                    {
-                        var report = ExecuteSelectFromKeyQuery(key, "GetRecordFromKey");
-                        if (report.State == "Consumed")
-                        {
-                            File.Create(AppDomain.CurrentDomain.BaseDirectory + "Estado.Consumed");
-                        }
-                        if (report.State == "Bound")
-                        {
-                            File.Create(AppDomain.CurrentDomain.BaseDirectory + "Estado.Bound");
-                        }
-                    }
-                    Environment.Exit(sqloutput);
-                }
-                catch (Exception e)
-                {
-                    if (e.Message.Contains("network-related"))
-                    {
-                        //Console.WriteLine("Error en la red intentando guardar registro." + Environment.NewLine + e.Message);
-                        Environment.Exit(-8);
-                    }
-                    //Console.WriteLine("Error intentando guardar registro." + Environment.NewLine + e.Message);
-                }
-                finally
-                {
-                    sqlConnection.Close();
-                }
-            }
-        }
 
         private static void ShowHelp()
         {
-            Console.WriteLine("Opciones de parámetros:");
             Console.WriteLine("");
-            Console.WriteLine("      /sk { ProductKey SerialNumber [ProductKeyID ProductKeyPartNumber] }");
-            Console.WriteLine("      Crea un nuevo registro en la base de datos con estado 'Consumed'");
+            Console.WriteLine(" Opciones:");
             Console.WriteLine("");
-            Console.WriteLine("      /hk { ProductKey SerialNumber }");
-            Console.WriteLine("      Cambia el estado 'Consumed' de un registro a 'Bound'");
+            //Console.WriteLine("      /sk { SerialNumber ProductKey ProductKeyID ProductKeyState ProductKeyPartNumber }");
+            //Console.WriteLine("      Crea un nuevo registro en la base de datos con estado 'Consumed'");
+            //Console.WriteLine("");
+            //Console.WriteLine("      /hk { SerialNumber ProductKey }");
+            //Console.WriteLine("      Cambia el estado de un registro de 'Consumed' a 'Bound'");
+            //Console.WriteLine("");
+            Console.WriteLine("      /SaveAssembledFile {Ruta al archivo XML} [Origen]");
+            Console.WriteLine("      Crea un nuevo registro en la base de datos con estado 'Consumed' a partir del archivo OA3.xml dado.");
+            Console.WriteLine("      El parámetro 'Origen' es opcional. Si no se especifíca se asigna el valor 'N/A'. Máximo 10 caracteres.");
+            Console.WriteLine("");
+            Console.WriteLine("      /SetAsBound { SerialNumber ProductKey }");
+            Console.WriteLine("      Cambia el estado de un registro de 'Consumed' a 'Bound' si ambos valores estan asociados en la base de datos.");
+            Console.WriteLine("");
+            Console.WriteLine("      /SaveReportedFile { SerialNumber ProductKey }");
+            Console.WriteLine("      Crea un nuevo registro en la base de datos del archivo OA3.xml reportado luego de crear el Hardware Hash.");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine(" Ejemplos:");
+            Console.WriteLine(@"      EXOKeyCheck /SaveAssembledFile C:\OA3\OA3.xml Microsoft");
+            Console.WriteLine(@"      EXOKeyCheck /SetAsBound C:\OA3\OA3.xml");
+            Console.WriteLine(@"      EXOKeyCheck /SaveReportedFile C:\OA3\OA3.xml");
             Console.WriteLine("");
             Console.WriteLine(" Códigos de salida generales:");
             Console.WriteLine("");
@@ -334,11 +350,11 @@ namespace EXOKeyCheck
             Console.WriteLine("");
             Console.WriteLine("     -2  = ProductKey ya existe en base de datos.");
             Console.WriteLine("     -3  = SerialNumber ya existe en base de datos.");
-            Console.WriteLine("     -5  = ProductKey y SerialNumber ya existen en base de datos.");
+            Console.WriteLine("     -5  = SerialNumber y ProductKey ya existen en base de datos.");
             Console.WriteLine("");
             Console.WriteLine(" Códigos de salida para el párametro '/hk':");
             Console.WriteLine("");
-            Console.WriteLine("     -4  = ProductKey y SerialNumber existen en la base de datos, pero no estan asociados.");
+            Console.WriteLine("     -4  = SerialNumber y ProductKey existen en la base de datos, pero no estan asociados.");
             Console.WriteLine("     -6  = No se encontró ProductKey en la base de datos.");
             Console.WriteLine("     -7  = No se encontró SerialNumber en la base de datos.");
             Console.WriteLine("     -9  = El el estado del registro en la base de datos es distinto de 'Consumed'.");
